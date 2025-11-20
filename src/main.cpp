237cace1b1c6b60e23c86e38717045903e63c003
Vadym Grabovenko <playstation4love@gmail.com>
2025-11-20 12:06:18 +0200

    Added user input, check if num < 0 and check if it is not a positive integer

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 #include "Bigint.hpp"
 
 int main() {
-    int n = 24;
-
+    int n;
+    std::cout << "Enter a num to find its factorial ->";
+    if  (!(std::cin >> n)) {
+        std::cout << "You've entered something that is not an integer." << std::endl;
+        return -1;
+    }
+    if (n < 0) {
+        std::cout << "Your num must be a positive integer to find a factorial" << std::endl;
+        return -1;
+    }
     bigint::Bigint result = bigint::Bigint::factorial(n);
     std::cout << n << "! = " << result << std::endl;
 
